96c55e41d8acc40bcb9370c2afc48eac89052545
Frankluisravlo@gmail.com <Frankluisravlo@gmail.com>
2021-01-28 18:31:13 -0500

    modified access to logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import logo from './image.ico';
 import './Logo.css';
+import Pdf from '../header/frankluisravelo.pdf';
 
 const Logo = () => {
     return (
       <div className="Logo-container">
-        <img src={logo} className="App-logo" alt="logo" />
+        <a href={Pdf} target = "_blank" rel="noopener noreferrer"><img src={logo} className="App-logo" alt="logo" /></a>
       </div>
     )
 }
