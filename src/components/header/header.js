bc6729a48b7e8aee861e49c880d08b4be5e57dbb
Frankluisravlo@gmail.com <Frankluisravlo@gmail.com>
2021-01-28 16:23:46 -0500

    Added social icos

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,13 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
 
-import './header.css';
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 
+import Linkedin from '../../svg/linkedin.svg';
+import Github from '../../svg/github.svg';
+import Mail from "../../svg/gmail.svg";
+import twitter from "../../svg/twitter.svg";
+// import JS from "../../svg/javascript.svg";
+// import Node from "../../svg/node.svg";
+// import reactLogo from "../../svg/react.svg";
+
+
 const Navigation = () => {
     return (
-    <Navbar className='nav' collapseOnSelect expand="lg" bg="ligth">
-      <Navbar.Brand className='brand' href="#home" style={{color: `#999`, fontSize: `2em`}}>Frank Luis-Ravelo</Navbar.Brand>
+    <MyNavbar collapseOnSelect expand="lg" bg="ligth">
+      <Navbar.Brand href="#home" style={{color: `#999`, fontSize: `2em`}}>Frank Luis-Ravelo</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">
@@ -16,15 +25,30 @@ const Navigation = () => {
           <Nav.Link href="#pricing" disabled>Disable</Nav.Link>
           */}
         </Nav>
-        <Nav style={{color: `#999`}}>
-        <p>Icon</p>
-        <p>Icon</p>
-        <p>Icon</p>
-        <p>Icon</p>
-        </Nav>
+        <Nav.Link href="https://www.linkedin.com/in/franklr" target="_blank" rel="noopener noreferrer"><Icon src={Linkedin} alt="Linkedin" />
+          </Nav.Link>
+
+        <Nav.Link href="https://www.github.com/frankazo" target="_blank" rel="noopener noreferrer"><Icon src={Github} alt="Linkedin" />
+          </Nav.Link>
+
+        <Nav.Link href="https://www.twitter.com/frankazolr" target="_blank" rel="noopener noreferrer"><Icon src={twitter} alt="Linkedin" />
+          </Nav.Link>
+
+        <Nav.Link href="mailto: Frankluisravlo@gmail.com"><Icon src={Mail} alt="Linkedin" />
+          </Nav.Link>
       </Navbar.Collapse>
-    </Navbar>
+    </MyNavbar>
     )
 }
+
+const MyNavbar = styled(Navbar)`
+  background-color: #282c34;
+  margin-bottom: -60px;
+  `
+
+const Icon = styled.img`
+  width: 40px;
+  height: auto;
+`
 
 export default Navigation
